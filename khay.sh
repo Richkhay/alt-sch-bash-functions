bb51c8476a47ae518b6434fb389936d11d30cbcd
Osobu Kayode <richiekhay1@gmail.com>
2024-12-30 12:38:17 +0100

    Khay.sh

---
+++ khay.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+who_am_i() {
+  echo "Hello, I am Kayode Osobu!"
+}
+
+fullname() {
+  echo "Fullname: Kayode Osobu Emmanuel"
+}
+
+bio() {
+  echo "Bio: I am a Network Marketer and Funnel Designer with a passion for building scalable and Website for small business owners. I have experience in building Wordpress webesite, Wix, Square Spacechnologies,I am also proficient in cloud technologies such as AWS and Azure. I am passionate about learning new technologies and solving complex problems."
+}
+
+year_in_tech() {
+  echo "Year in Tech: I have been in tech for 3 years but as an entrepreneur for 5 years."
+}
+
+dream_job() {
+  echo "Dream Job: cloud Engineer and Tech Investor."
+}
